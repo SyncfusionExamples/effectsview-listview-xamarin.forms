4b520446b6ca035d6e0fe1a378359ab8669280af
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-02-26 13:03:58 +0530

    XAMARIN-33593 - Sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.XForms.EffectsView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<SfEffectsView>
+    {
+        protected override void OnAttachedTo(SfEffectsView bindable)
+        {
+            bindable.SelectionChanged += Bindable_SelectionChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void Bindable_SelectionChanged(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var effectsView = sender as SfEffectsView;
+                effectsView.ScaleFactor = 0.85;
+                effectsView.ApplyEffects(SfEffects.Scale);
+            });
+        }
+        protected override void OnDetachingFrom(SfEffectsView bindable)
+        {
+            bindable.SelectionChanged -= Bindable_SelectionChanged;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
